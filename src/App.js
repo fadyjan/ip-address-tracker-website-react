426eae2c20e778700b0028b7c42693faa658b493
fadyjan <fady.jan95@gmail.com>
2023-02-25 01:37:59 +0200

    Create #5 Application final touches

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header/Header';
+import Map from './components/Map/Map'
+import { useState } from 'react';
 function App() {
+  const [dataLoaded, setDataLoaded] = useState([])
+  const HandlerFunction  = (ChildToParentConst) => {
+    setDataLoaded(ChildToParentConst )
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header onSaveHandler  = {HandlerFunction  }></Header>
+      <Map ParentToChildData= {dataLoaded}></Map>
     </div>
   );
 }
