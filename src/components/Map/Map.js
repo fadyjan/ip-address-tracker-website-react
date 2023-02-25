dbdce20364f9ee64a6969faeabfa89ba263aff1a
fadyjan <fady.jan95@gmail.com>
2023-02-25 19:32:18 +0200

    Fix #1 Marker Positions On change

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,38 +3,35 @@ import "./Map.css";
 import GoogleMapReact from "google-map-react";
 import MyMarker from "../MyMarker/MyMarker";
 export default function Map(props) {
-  const [mapLocation, setMapLocation] = useState(false);
+  const [mapLocation, setMapLocation] = useState({
+    center: [17, 18],
+    zoom: 12,
+  });
 
   useEffect(() => {
     if (props.ParentToChildData.length != 0) {
-      setMapLocation(props.ParentToChildData.location);
+      setMapLocation({
+        center: [
+          props.ParentToChildData.location.lat,
+          props.ParentToChildData.location.lng,
+        ],zoom:12
+      });
     }
   }, [props]);
-  let defaultProps = {
-    center: {
-      lat: 17,
-      lng: 18,
-    },
-    zoom: 0,
-  };
 
   return (
     // Important! Always set the container height explicitly
     <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: "" }}
-        defaultCenter={
-          mapLocation
-            ? { "lat": mapLocation.lat, "lng": mapLocation.lng }
-            : defaultProps.center
-        }
-        defaultZoom={defaultProps.zoom}
+        defaultZoom={mapLocation.zoom}
+        center={mapLocation.center}
       >
         {mapLocation && (
           <MyMarker
             key={1}
-            lat={mapLocation.lat}
-            lng={mapLocation.lng}
+            lat={mapLocation.center[0]}
+            lng={mapLocation.center[1]}
             text={1}
             tooltip={"Hello"}
           />
